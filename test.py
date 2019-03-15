bf096b97bbda58d6db5e876f3340c11247aaca15
Chris Beldam <chrisgbeldam@gmail.com>
2019-03-15 19:24:19 +0000

    Starting of stats counter

--- test.py
+++ test.py
@@ -23,13 +23,30 @@ class Components:
     def new_blocks(self):
         return random.choice(self.blocks) # Returns a new block either A, B or ""
 
-#Create Conveyor Belt
-class ConveyorBelt:
-    """ Class which controls the conveyorbelt and its functionality """
-    def __init__(self):
-    #Build conveyor belt process - move over blocks by 1 each time for 100 seconds
+#Position on conveyorbelt (had to look up how to do this)
+class Position:
+    def __init__(self, val):
+        self.val = val #Value which represents conveyorbelt position
 
 class Statistics:
     """ Class which keeps track of the statistics of which items got picked """
     def __init__(self):
-        self.totals = {'A':0, 'B':0, "P":0} #Dictionary to track how many values of each occur
+        self.totals = {'A':0, 'B':0, "P":0} #Dictionary to track how many values of each occur and set beginning values at 0
+    
+    def tracker(self, output):
+        if len(output.val) > 0:
+            key = output.val[0]
+            self.data[key] += 1 #adds one to data value
+            
+    def tracking_report(self):
+        total_unpicked = self.data['A'] + self.data['B']:
+        print(f"Total Products: {self.data['P']}")
+        print(f'Total Unpicked: {total_unpicked}')
+     
+#Create Conveyor Belt
+class ConveyorBelt:
+    """ Class which controls the conveyorbelt and its functionality """
+    def __init__(self,position):
+        self.position = position
+    #Build conveyor belt process - move over blocks by 1 each time for 100 seconds
+
